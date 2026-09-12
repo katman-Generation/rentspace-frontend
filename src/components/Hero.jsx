9fb8fdd8304384e48d0ac089ebef7fdb32cb5078
Tatenda  Katema <primewebwizards@gmail.com>
2026-09-12 02:20:05 +0200

    making our front end look more premium

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import heroImage from "../assets/rentspace-hero.jpg";
+import heroImage from "../assets/rentspace-hero.jpeg";
 
 export default function Hero() {
   return (
